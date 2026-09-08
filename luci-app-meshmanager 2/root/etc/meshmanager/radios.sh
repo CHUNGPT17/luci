0b8011390c3cde6893a7b56267068f628decd262
CHUNGPT17 <116159227+CHUNGPT17@users.noreply.github.com>
2026-09-08 14:56:10 +0700

    Delete luci-app-meshmanager 2 directory

--- luci-app-meshmanager 2/root/etc/meshmanager/radios.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-. /etc/meshmanager/lib.sh
-printf '{"radio24":"%s","radio5":"%s"}\n' "$(mm_find_radio 2g)" "$(mm_find_radio 5g)"
--- luci-app-meshmanager 2/root/etc/meshmanager/sync-status.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-printf '{"version":"%s","last_sync":"%s"}\n' "$(uci -q get meshmanager.main.config_version || echo 1)" "$(cat /tmp/meshmanager/last_sync 2>/dev/null)"
